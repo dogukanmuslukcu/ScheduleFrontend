fa68a18fcf2581d54e961752733205b844413c88
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-12-20 18:15:37 +0300

    committed

--- src/app/models/meeting/meeting.ts
+++ src/app/models/meeting/meeting.ts
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 export interface Meeting{
     MeetingTime:string;
     HostUser :string;
     MeetingPlace:string;
   }
+=======
+export interface Meeting{
+    MeetingTime:string;
+    HostUser :string;
+    MeetingPlace:string;
+  }
+>>>>>>> 2496fbc (committed)
   
